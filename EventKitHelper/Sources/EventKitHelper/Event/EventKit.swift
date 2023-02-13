f6955c1df2931ae023f7c8564e0bbf72ce7c7967
David Obwaller <david.obwaller@gmail.com>
2023-02-13 15:59:52 +0100

    Float code

--- EventKitHelper/Sources/EventKitHelper/Event/EventKit.swift
+++ EventKitHelper/Sources/EventKitHelper/Event/EventKit.swift
@@ -2,21 +2,25 @@ import EventKit
 
 extension Event {
     init(ekEvent: EKEvent) {
+        let timeZone = ekEvent.timeZone.flatMap { tz in _TimeZone(timeZone: tz) }
+
         let title = ekEvent.structuredLocation?.title
         let coordinate = ekEvent.structuredLocation?.geoLocation?.coordinate
+        let location = title.flatMap { t in
+            Location(
+                title: t,
+                coordinate: coordinate.flatMap{
+                    c in Coordinate(clLocationCoordinate2D: c)
+                }
+            )
+        }
+
         self.init(
             title: ekEvent.title,
             startDate: ekEvent.startDate,
             endDate: ekEvent.endDate,
-            timeZone: ekEvent.timeZone.flatMap { tz in _TimeZone(timeZone: tz) },
-            location: title.flatMap { t in
-                Location(
-                    title: t,
-                    coordinate: coordinate.flatMap{
-                        c in Coordinate(clLocationCoordinate2D: c)
-                    }
-                )
-            }
+            timeZone: timeZone,
+            location: location
         )
     }
 }

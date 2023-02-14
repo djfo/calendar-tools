f8a596a4ff3968aa619d1853f2b4ad355c991920
David Obwaller <david.obwaller@gmail.com>
2023-02-14 10:29:55 +0100

    Alternatively take start/end date as command-line arguments

--- EventKitHelper/Sources/EventKitHelper/main.swift
+++ EventKitHelper/Sources/EventKitHelper/main.swift
@@ -1,13 +1,30 @@
 import Foundation
 
+enum InputError: Error {
+    case invalidArguments
+}
+
 do {
-    let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .iso8601
-    let data = FileHandle.standardInput.availableData
-    let decoded = try decoder.decode(Input.self, from: data)
+    let arguments = CommandLine.arguments
+
+    let input: Input
+    if arguments.count == 3 {
+        let rawStartDate = arguments[1]
+        let rawEndDate = arguments[2]
+        let formatter = ISO8601DateFormatter()
+        guard
+            let startDate = formatter.date(from: rawStartDate),
+            let endDate = formatter.date(from: rawEndDate)
+        else {
+            throw InputError.invalidArguments
+        }
+        input = Input(predicate: Predicate(startDate: startDate, endDate: endDate))
+    } else {
+        input = try decodeInput()
+    }
 
     let helper = EventKitHelper()
-    helper.run(predicate: decoded.predicate)
+    helper.run(predicate: input.predicate)
 
     autoreleasepool {
         RunLoop.main.run()
@@ -15,4 +32,11 @@ do {
 } catch {
     print("Failed to parse input.")
     exit(1)
+}
+
+func decodeInput() throws -> Input {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    let data = FileHandle.standardInput.availableData
+    return try decoder.decode(Input.self, from: data)
 }
